29bc97f747d308653227b2649270ab76902ec9dc
nathanalbano1 <MallariAb21@gmail.com>
2025-01-12 16:33:53 -0800

    Add files via upload

---
+++ code.py
@@ -0,0 +1,16 @@
+# Save on CPX/CPB as code.py
+# Blinks the top right RED LED
+import board
+import digitalio
+import time
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    time.sleep(0.0001)
+    led.value = False
+    time.sleep(0.1)
+
+print("hello world!")
