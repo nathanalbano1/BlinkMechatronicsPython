f000f7e813a55455e97a2ccf3f1f94402e1f67f7
nathanalbano1 <MallariAb21@gmail.com>
2025-02-14 10:00:20 -0800

    Add files via upload

---
+++ dicts_mechatronics.py
@@ -0,0 +1,26 @@
+# Python Lab - Scratch Pad
+2	# Use this template to try some code... Use it as a scratch pad.
+3	# Remember To Copy Your Edited Code To Your GitHub
+4	# https://github.com/
+5	
+6	# Add Any Header Comments, Versioning & License
+7	# Add a Comment Here to describe/explain what you are doing 
+8	# What is the purpose of this code… Document It
+9	
+10	# Your Code Starts Here:
+11	# Include any Libraries
+12	# Declare Any Global Variables
+13	
+14	def main():
+15	  print("Starting The Code Challenge")
+16	  # Declare Any main() Variables - (Not Global)
+17	
+18	  print("Starting <Specific Part> Of Code Challenge")
+19	  # Enter Your Code Here:
+20	  print('Enter Your Code Here:')
+21	  x = 42
+22	  print(x)
+23	  print("Ending <Specific Part> Of Code Challenge")
+24	  print("Ending The Code Challenge")
+25	
+26	main()
